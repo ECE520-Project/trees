c2062f1e87ebd4c06bbf219c76c4c0d0e024779c
Amina Eltahir <amina.a.ibrahim22@gmail.com>
2020-11-17 23:09:52 -0700

    added balance & rotation

--- src/avltree.rs
+++ src/avltree.rs
@@ -1,6 +1,7 @@
 use std::cell::RefCell;
 use std::rc::Rc;
 use std::fmt;
+use std::mem::{replace, swap};
 
 use crate::base::{QueryableTreeNode, QueryableTree};
 
@@ -58,9 +59,97 @@ impl<T: Ord + Copy + fmt::Debug> AVLTreeNode<T> {
             _ => {},
         }
     }
+    //balance factor
+    fn balance_factor(&self) -> i8 {
+        let left_height = self.get_left().as_ref().map(
+            |l| l.borrow().height()
+        ).unwrap_or(0);
+        let right_height = self.get_right().as_ref().map(
+            |r| r.borrow().height()
+        ).unwrap_or(0);
 
+        if left_height >= right_height {
+            (left_height - right_height) as i8
+        } else {
+            -((right_height - left_height) as i8)
+        }
+    }
+    //rotate left to balance the tree
+    fn rotate_left(&mut self) -> bool {
+        if self.right.is_none() {
+            return false;
+        }
+        let right_node = self.right.as_mut().unwrap();
+        let right_left_tree = right_node.borrow_mut().left.take();
+        let right_right_tree = right_node.borrow_mut().right.take();
+
+        let mut new_left_tree = replace(&mut self.right, right_right_tree);
+        swap(&mut self.data, &mut new_left_tree.as_mut().unwrap().borrow_mut().data);
+        let left_tree = self.left.take();
+
+        let new_left_node = new_left_tree.as_mut().unwrap();
+        new_left_node.borrow_mut().right = right_left_tree;
+        new_left_node.borrow_mut().left = left_tree;
+        self.left = new_left_tree;
+
+        if let Some(node) = self.left.as_mut() {
+            node.borrow_mut().height();
+        }
+        self.height();
+        true
+    }
+    //rotate left function
+    pub fn rotate_right(&mut self) -> bool {
+        if self.left.is_none() {
+            return false;
+        }
+        let left_node = self.left.as_mut().unwrap();
+        let left_right_tree = left_node.borrow_mut().right.take();
+        let left_left_tree = left_node.borrow_mut().left.take();
+
+        let mut new_right_tree = replace(&mut self.left, left_left_tree);
+        swap(&mut self.data, &mut new_right_tree.as_mut().unwrap().borrow_mut().data);
+        let right_tree = self.right.take();
+
+        let new_right_node = new_right_tree.as_mut().unwrap();
+        new_right_node.borrow_mut().left = left_right_tree;
+        new_right_node.borrow_mut().right = right_tree;
+        self.right = new_right_tree;
+
+        if let Some(node) = self.right.as_mut() {
+            node.borrow_mut().height();
+        }
+        self.height();
+        true
+    }
+    //rebalance after deletion
+    fn rebalance(&mut self) -> bool {
+        match self.balance_factor() {
+            -2 => {
+                let right_node = self.right.as_mut().unwrap();
+
+                if right_node.borrow_mut().balance_factor() == 1 {
+                    right_node.borrow_mut().rotate_right();
+                }
+                self.rotate_left();
+                true
+            }
+            2 => {
+                let left_node = self.left.as_mut().unwrap();
+                if left_node.borrow_mut().balance_factor() == -1 {
+                    left_node.borrow_mut().rotate_left();
+                }
+                self.rotate_right();
+                true
+            }
+            _ => false,
+        }
+    }
+    //delete node then use the balance function
+    fn delete(&mut self, node: AVLTreeNode<T>){
+        unimplemented!()
+    }
 }
-
 impl<T: Ord + Copy + fmt::Debug> AVLTree<T> {
     // pub fn new() -> Self {
     //     Self { root: None }
@@ -76,14 +165,12 @@ impl<T: Ord + Copy + fmt::Debug> AVLTree<T> {
             })))
         }
     }
-
     pub fn insert(&mut self, val: T){
         match self.root {
             None => self.root = Some(Rc::new(RefCell::new(AVLTreeNode::new(val)))),
             Some(ref mut r) => r.borrow_mut().insert(AVLTreeNode::new(val)),
         }
     }
-
     pub fn delete(&self, val: T) {
         unimplemented!()
     }
