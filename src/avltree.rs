2b7df038689429f693ef6d458df4eef6be954f06
Amina Eltahir <amina.a.ibrahim22@gmail.com>
2020-11-26 14:20:11 -0700

    new avl delete

--- src/avltree.rs
+++ src/avltree.rs
@@ -8,7 +8,7 @@
 //! use trees::base::QueryableTree;
 //! ```
 
-use core::cmp::max;
+use std::cmp;
 use std::cell::RefCell;
 use std::rc::Rc;
 use std::fmt;
@@ -27,7 +27,8 @@ pub struct AVLTreeNode<T: Ord + Copy + fmt::Debug> {
     parent: AVLNodeLink<T>,
     left: AVLNodeLink<T>,
     right: AVLNodeLink<T>,
-    height: usize,
+    //height: usize,
+    height:u32,
 }
 
 /// An implementation of [AVL Tree](https://en.wikipedia.org/wiki/AVL_tree)
@@ -56,7 +57,6 @@ impl<T: Ord + Copy + fmt::Debug> AVLTreeNode<T> {
             height: 0,
         }
     }
-
     /// Insert a new node
     fn insert(&mut self, node: AVLTreeNode<T>) {
         match (node).data.cmp(&self.data) {
@@ -74,112 +74,120 @@ impl<T: Ord + Copy + fmt::Debug> AVLTreeNode<T> {
             },
             _ => {},
         }
-        self.rebalance();
+    }
+    /// Delete a node
+    fn delete(data: T, mut root: RcRefAVLTNode<T>) -> AVLNodeLink<T>{
+        match root.borrow_mut().data.cmp(&data){
+            Ordering::Equal =>  return Self::delete_node(root.clone()),
+            Ordering::Less => {
+                if let Some(succ) = root.borrow_mut().right.take() {
+                    root.borrow_mut().right = Self::delete(data, succ);
+                    return Some(Self::updated_node(root.clone()))
+                }
+            },
+            Ordering::Greater => {
+                if let Some(succ) = root.borrow_mut().left.take() {
+                    root.borrow_mut().left =  Self::delete(data, succ);
+                    return Some(Self::updated_node(root.clone()))
+                }
+            }
+        }
+        return Some(root);
+    } 
+    fn delete_node(mut node: RcRefAVLTNode<T>) -> AVLNodeLink<T> {
+        match node.borrow_mut().left.take(){
+            None => match node.borrow_mut().right.take(){
+                None => None,
+                Some(r) => Some(r),
+            }
+            Some(l) => match node.borrow_mut().right.take(){
+                None => Some(l),
+                Some(r) => Some(Self::combine_tree(l,r)),
+            }
+        }
     }
     
-    /// Get height of left child
-    fn left_height(&self) -> usize {
-        self.left.as_ref().map_or(0, |left| left.borrow().height())
+    fn height(node: &AVLNodeLink<T>) -> u32  {
+        return node.as_ref().map_or(0, |succ| succ.borrow().height)
     }
-
-    /// Get height of right child
-    fn right_height(&self) -> usize {
-        self.right.as_ref().map_or(0, |right| right.borrow().height())
+    fn update_height(node:&mut AVLTreeNode<T>){
+        node.height = cmp::max( Self::height(&node.left), Self::height(&node.right) )+1;
     }
-
-    /// Update height for current node based on children's height
-    fn update_height(&mut self) {
-        self.height = 1 + max(self.left_height(), self.right_height());
+    fn rotate_right(mut root: RcRefAVLTNode<T>) -> RcRefAVLTNode<T>{
+        let new_root = root.as_ref().borrow_mut().left.take().expect("broken tree");
+        root.as_ref().borrow_mut().left = new_root.as_ref().borrow_mut().right.take();
+        Self::update_height(&mut root.as_ref().borrow_mut());
+        new_root.borrow_mut().right = Some(root);
+        Self::update_height(&mut new_root.as_ref().borrow_mut());
+        return new_root
     }
-
-    /// Calculate balance factor for current node
-    fn balance_factor(&self) -> i8 {
-        let left_height = self.get_left().as_ref().map(
-            |l| l.borrow().height()
-        ).unwrap_or(0);
-        let right_height = self.get_right().as_ref().map(
-            |r| r.borrow().height()
-        ).unwrap_or(0);
-
-        if left_height >= right_height {
-            (left_height - right_height) as i8
-        } else {
-            -((right_height - left_height) as i8)
+    fn rotate_left(mut root: RcRefAVLTNode<T>) -> RcRefAVLTNode<T>{
+        let mut new_root = root.as_ref().borrow_mut().right.take().expect("not accepted");
+        root.as_ref().borrow_mut().right = new_root.as_ref().borrow_mut().left.take();
+        Self::update_height(&mut root.as_ref().borrow_mut());
+        new_root.borrow_mut().left = Some(root);
+        Self::update_height(&mut new_root.as_ref().borrow_mut());
+        return new_root
+    }
+    fn rotate_right_node(mut root: RcRefAVLTNode<T>) -> RcRefAVLTNode<T> {
+        let right = root.as_ref().borrow_mut().right.take().expect("not accepted");
+        if Self::height(&right.borrow_mut().left) > Self::height(&right.borrow_mut().right) {
+            let rotated_node = Self::rotate_right(right);
+            root.as_ref().borrow_mut().right = Some(rotated_node);
+            Self::update_height(&mut root.as_ref().borrow_mut());
+        }
+        else {
+            root.as_ref().borrow_mut().right = Some(right)
+        }
+        Self::rotate_left(root)
+    }
+    fn rotate_left_node(mut root: RcRefAVLTNode<T>) -> RcRefAVLTNode<T> {
+        let left = root.as_ref().borrow_mut().left.take().expect("not accepted");
+        if Self::height(&left.borrow_mut().left) < Self::height(&left.borrow_mut().right) {
+            let rotated_node = Self::rotate_left(left);
+            root.as_ref().borrow_mut().left = Some(rotated_node);
+            Self::update_height(&mut root.as_ref().borrow_mut());
+        }
+        else{
+            root.as_ref().borrow_mut().left = Some(left);
+        }
+        Self::rotate_right(root)
+    }
+    fn diff_height(root: &RcRefAVLTNode<T>) -> i32 {
+        let left_height = Self::height(&root.as_ref().borrow_mut().left);
+        let right_height = Self::height(&root.as_ref().borrow_mut().right);
+        (left_height as i32) - (right_height as i32)
+    }
+    fn rebalance(root: RcRefAVLTNode<T>) -> RcRefAVLTNode<T> {
+        let diff  = Self::diff_height(&root);
+        if -1 <= diff && diff <= 1 {return root}
+        match diff{
+            2 => Self::rotate_left_node(root),
+            -2 => Self::rotate_right_node(root),
+            _ => unreachable!()
         }
     }
-
-    /// Rotate left to balance the tree
-    fn rotate_left(&mut self) -> bool {
-        if self.right.is_none() {
-            return false;
-        }
-        let right_node = self.right.as_mut().unwrap();
-        let right_left_tree = right_node.borrow_mut().left.take();
-        let right_right_tree = right_node.borrow_mut().right.take();
-
-        let mut new_left_tree = replace(&mut self.right, right_right_tree);
-        swap(&mut self.data, &mut new_left_tree.as_mut().unwrap().borrow_mut().data);
-        let left_tree = self.left.take();
-
-        let new_left_node = new_left_tree.as_mut().unwrap();
-        new_left_node.borrow_mut().right = right_left_tree;
-        new_left_node.borrow_mut().left = left_tree;
-        self.left = new_left_tree;
-
-        if let Some(node) = self.left.as_mut() {
-            node.borrow_mut().update_height();
-        }
-        self.update_height();
-        true
+    fn updated_node(root: RcRefAVLTNode<T>) ->RcRefAVLTNode<T> {
+        Self::update_height(&mut root.as_ref().borrow_mut());
+        Self::rebalance(root)
     }
-
-    /// Rotate right to balance the tree
-    pub fn rotate_right(&mut self) -> bool {
-        if self.left.is_none() {
-            return false;
-        }
-        let left_node = self.left.as_mut().unwrap();
-        let left_right_tree = left_node.borrow_mut().right.take();
-        let left_left_tree = left_node.borrow_mut().left.take();
-
-        let mut new_right_tree = replace(&mut self.left, left_left_tree);
-        swap(&mut self.data, &mut new_right_tree.as_mut().unwrap().borrow_mut().data);
-        let right_tree = self.right.take();
-
-        let new_right_node = new_right_tree.as_mut().unwrap();
-        new_right_node.borrow_mut().left = left_right_tree;
-        new_right_node.borrow_mut().right = right_tree;
-        self.right = new_right_tree;
-
-        if let Some(node) = self.right.as_mut() {
-            node.borrow_mut().update_height();
-        }
-        self.update_height();
-        true
+    fn delete_left_node(mut root: RcRefAVLTNode<T>, left: RcRefAVLTNode<T>) -> (AVLNodeLink<T>,RcRefAVLTNode<T>){
+        let (new_left, min) =  Self::delete_min(left);
+        root.as_ref().borrow_mut().left = new_left;
+        (Some(Self::updated_node(root)),min)
     }
-
-    /// Rebalance after deletion
-    fn rebalance(&mut self) -> bool {
-        match self.balance_factor() {
-            -2 => {
-                let right_node = self.right.as_mut().unwrap();
-
-                if right_node.borrow_mut().balance_factor() == 1 {
-                    right_node.borrow_mut().rotate_right();
-                }
-                self.rotate_left();
-                true
-            }
-            2 => {
-                let left_node = self.left.as_mut().unwrap();
-                if left_node.borrow_mut().balance_factor() == -1 {
-                    left_node.borrow_mut().rotate_left();
-                }
-                self.rotate_right();
-                true
-            }
-            _ => false,
+    fn delete_min(mut root: RcRefAVLTNode<T>) -> (AVLNodeLink<T>, RcRefAVLTNode<T>) {
+        match root.borrow_mut().left.take() {
+            Some(left) => Self::delete_left_node(root.clone(), left),
+            None => (root.borrow_mut().right.take(), root.clone())
         }
+    }
+    fn combine_tree(l: RcRefAVLTNode<T>, r: RcRefAVLTNode<T>) -> RcRefAVLTNode<T>{
+        let (tree, min) = Self::delete_min(r);
+        let mut new_root = min;
+        new_root.borrow_mut().left = Some(l);
+        new_root.borrow_mut().right = tree;
+        Self::updated_node(new_root)
     }
 }
 
@@ -222,114 +230,10 @@ impl<T: Ord + Copy + fmt::Debug> AVLTree<T> {
     ///
     /// avl.delete(1);
     /// ```
-    pub fn delete(&self, data: T) {
-        unimplemented!();
-        // self.remove(&data);
+    pub fn delete(&mut self, val:T){
+        match self.root.take() {
+            Some(node) => self.root = AVLTreeNode::delete(val, node),
+            None => return
+        }
     }
-    
-    // pub fn remove(&self, data: &T) -> Option<T> {
-    //     let mut prev_ptrs = Vec::<*mut AVLTreeNode<T>>::new();
-    //     let mut current_tree = &self.root;
-    //     let mut target_value = None;
-    //
-    //     while let Some(current_node) = current_tree {
-    //         match current_node.borrow().data.cmp(&data) {
-    //             Ordering::Less => {
-    //                 prev_ptrs.push(&mut *current_node.borrow());
-    //                 current_tree = &mut current_node.borrow().right;
-    //             }
-    //             Ordering::Equal => {
-    //                 target_value = Some(&mut *current_node);
-    //                 break;
-    //             }
-    //             Ordering::Greater => {
-    //                 prev_ptrs.push(&mut *current_node.borrow());
-    //                 current_tree = &mut current_node.borrow().left;
-    //             }
-    //         };
-    //     }
-    //     if target_value.is_none() {
-    //         return None;
-    //     }
-    //     let mut target_node = target_value.unwrap();
-    //
-    //     let taken_value = if target_node.as_ref().borrow().left.is_none() || target_node.as_ref().borrow().right.is_none() {
-    //         if let Some(left_node) = target_node.as_ref().borrow_mut().left.take() {
-    //             replace(target_node, *left_node).borrow_mut().data
-    //         } else if let Some(right_node) = target_node.as_ref().borrow_mut().right.take() {
-    //             replace(target_node, *right_node).borrow_mut().data
-    //         } else if let Some(prev_ptr) = prev_ptrs.pop() {
-    //             let prev_node = unsafe {&mut *prev_ptr };
-    //
-    //             let inner_value = if let Some(left_node) = prev_node.left.as_ref() {
-    //                 if left_node.borrow_mut().data == target_node.borrow_mut().data {
-    //                     prev_node.left.take().unwrap().borrow_mut().data
-    //                 } else {
-    //                     prev_node.right.take().unwrap().borrow_mut().data
-    //                 }
-    //             } else {
-    //                 prev_node.right.take().unwrap().borrow_mut().data
-    //             };
-    //
-    //             prev_node.update_height();
-    //             prev_node.rebalance();
-    //
-    //             inner_value
-    //         } else {
-    //             self.root.take().unwrap().borrow_mut().data
-    //         }
-    //     } else {
-    //         let right_tree = &mut target_node.borrow_mut().right;
-    //
-    //         if right_tree.as_ref().unwrap().borrow_mut().left.is_none() {
-    //             let mut right_node = right_tree.take().unwrap();
-    //
-    //             let inner_value = replace(&mut target_node.borrow_mut().data, right_node.borrow_mut().data);
-    //             replace(&mut target_node.borrow_mut().right, right_node.borrow_mut().right.take());
-    //
-    //             target_node.borrow_mut().update_height();
-    //             target_node.borrow_mut().rebalance();
-    //
-    //             inner_value
-    //         } else {
-    //             let mut next_tree = right_tree;
-    //             let mut inner_ptrs = Vec::<*mut AVLTreeNode<T>>::new();
-    //
-    //             while let Some(next_left_node) = next_tree {
-    //                 if next_left_node.borrow_mut().left.is_some() {
-    //                     inner_ptrs.push(&mut *next_left_node.borrow_mut());
-    //                 }
-    //                 next_tree = &mut next_left_node.borrow_mut().left;
-    //             }
-    //
-    //             let parent_left_node = unsafe { &mut *inner_ptrs.pop().unwrap() };
-    //             let mut leftmost_node = parent_left_node.left.take().unwrap();
-    //
-    //             let inner_value = replace(&mut target_node.borrow_mut().data, leftmost_node.borrow_mut().data);
-    //             replace(&mut parent_left_node.left, leftmost_node.borrow_mut().right.take());
-    //
-    //             parent_left_node.update_height();
-    //             parent_left_node.rebalance();
-    //
-    //             for node_ptr in inner_ptrs.into_iter().rev() {
-    //                 let node = unsafe { &mut *node_ptr };
-    //                 node.update_height();
-    //                 node.rebalance();
-    //             }
-    //
-    //             target_node.borrow_mut().update_height();
-    //             target_node.borrow_mut().rebalance();
-    //
-    //             inner_value
-    //         }
-    //     };
-    //
-    //     for node_ptr in prev_ptrs.into_iter().rev() {
-    //         let node = unsafe { &mut *node_ptr };
-    //         node.update_height();
-    //         node.rebalance();
-    //     }
-    //
-    //     Some(taken_value)
-    // }
 }
